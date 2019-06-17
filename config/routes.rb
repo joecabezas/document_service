e85b969496977a7ef4a3559ac4f35f73c16d42a0
Joe Cabezas Campos <joe.cabezas@gmail.com>
2019-06-17 02:21:24 -0700

    added routes for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root controller: :documents, action: :index
   resources :documents do
     get 'versions/new'
     post 'versions', controller: :versions, action: :create
